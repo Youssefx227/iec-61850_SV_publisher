b0f2527feec0df75602aae1d8cbc15ad90af1355
youssef mounkaila <youssefmounkaila@gmail.com>
2019-04-11 14:38:14 +0200

    un autre commit

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
 
+	
+	
+	
 CC=gcc
 CFLAGS=-Wall -O -lpthread -lm
 LDFLAGS=-static
@@ -6,12 +9,25 @@ LIBPATH = -L../libiec61850-1.3.3/build/
 LIB = -liec61850
 EXEC=sv_publisher
 INCLUDE = -I../inc
+LIBIEC_HOME = /home/nathan/Documents/youssef_C/libiec61850-1.3.3/
 
+PROJECT_SOURCES = sv_publisher_example.c
+PROJECT_SOURCES += static_model.c
+
+PROJECT_ICD_FILE = sv.icd
+
+include /home/nathan/Documents/youssef_C/libiec61850-1.3.3/make/target_system.mk
+include /home/nathan/Documents/youssef_C/libiec61850-1.3.3/make/stack_includes.mk
 
 all: $(EXEC)
-$(EXEC): sv_publisher_example.c
-				$(CC) $(INCLUDE)  sv_publisher_example.c -o $(EXEC) $(CFLAGS) $(LIBPATH) $(LIB) 
+
+include $(LIBIEC_HOME)/make/common_targets.mk
+
+model:	$(PROJECT_ICD_FILE)
+	java -jar $(LIBIEC_HOME)/tools/model_generator/genmodel.jar $(PROJECT_ICD_FILE)
+
+$(EXEC): $(PROJECT_SOURCES)
+				$(CC) $(INCLUDE) $(PROJECT_SOURCES)  -o $(EXEC) $(CFLAGS) $(LIBPATH) $(LIB) 
 
 clean:
 	rm *.o
-
--- sv_publisher_example.c
+++ sv_publisher_example.c
@@ -1,7 +1,8 @@
-
 #include <iec61850_server.h>
+#include <iec61850_model.h>
 #include <sv_publisher.h>
 #include <hal_thread.h>
+#include <static_model.h>
 #include <signal.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -11,6 +12,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/syscall.h>
+#include <sys/time.h>
 #include <linux/types.h>
 #include <linux/sched.h>
 #include <pthread.h>
@@ -26,7 +28,6 @@
  #define __NR_sched_setattr		314
  #define __NR_sched_getattr	    315
  #endif
-
 
  static volatile int done;
 
@@ -72,8 +73,10 @@ SCHED_RR) */
 typedef struct data_{
 
     char* interface;
-    float* f_nominal,* samplesPerCycle,* fech,* f,* w,* Ueff,* Vamp,* Zmag,* Iamp,* phase,* n;
-    double* theta,* Va,* Vb,* Vc,* Vn,* ia,* ib,* ic,* in;
+    float* f_nominal,* samplesPerCycle,* fech,* f,* w,* phase,* n;
+    int*   Va,* Vb,* Vc,* Vn,* ia,* ib,* ic,* in, *Vamp , *Iamp;
+   // float*   Va,* Vb,* Vc,* Vn,* ia,* ib,* ic,* in, *Vamp , *Iamp;
+    double* theta ;
     pthread_mutex_t mutex;
 
 
@@ -98,138 +101,128 @@ void *publish (void *donnees){
     si l'allocation a marché, notre pointeur contient une adresse
     L'allocation dynamique permet notamment de créer un entier/float/tableau dont
     la taille est déterminée par une variable au moment de l'exécution */
-     data_* param = malloc(sizeof(data_));
+    data_* param = malloc(sizeof(data_));
     param = donnees;
-    double* Va = malloc(sizeof(double));
+    int* Va = malloc(sizeof(int));
      Va = (*param).Va;
-    double* ia = malloc(sizeof(double));
-    ia = (*param).ia;
-    double* Vb = malloc(sizeof(double));
-    Vb = (*param).Vb;
-    double* ib = malloc(sizeof(double));
-    ib = (*param).ib;
-    double* Vc = malloc(sizeof(double));
-    Vc = (*param).Vc;
-    double* ic = malloc(sizeof(double));
-    ic = (*param).ic;
-    double* Vn = malloc(sizeof(double));
-    Vn = (*param).Vn;
-    double* in = malloc(sizeof(double));
-    in = (*param).in;
+    int* ia = malloc(sizeof(int));
+     ia = (*param).ia;
+     int* Vb = malloc(sizeof(int));
+     Vb = (*param).Vb;
+    int* ib = malloc(sizeof(int));
+     ib = (*param).ib;
+    int* Vc = malloc(sizeof(int));
+     Vc = (*param).Vc;
+    int* ic = malloc(sizeof(int));
+     ic = (*param).ic;
+    int* Vn = malloc(sizeof(int));
+     Vn = (*param).Vn;
+    int* in = malloc(sizeof(int));
+     in = (*param).in;
     float* n = malloc(sizeof(float));
-    n = (*param).n;
+     n = (*param).n;
     char* interface = malloc(sizeof(char));
-    interface =(*param).interface ;
-    Quality q= QUALITY_VALIDITY_GOOD;
+     interface =(*param).interface ;
+
+    Quality q = QUALITY_VALIDITY_GOOD;
 
 
     SVPublisher svPublisher  = SVPublisher_create(NULL,interface); // Crée un nouveau publisher de sampled values selon l'IEC61850-9-2
-    SVPublisher_ASDU asdu1 = SVPublisher_addASDU(svPublisher, "svpub1isher", NULL,1); // création d'un bloc de donnée qui sera générer
+    SVPublisher_ASDU asdu1 = SVPublisher_addASDU(svPublisher, "svpub1isher", NULL, 1); // création d'un bloc de donnée qui sera générer
 
     // allocation mémoire dans le bloc de données
     int Ima = SVPublisher_ASDU_addINT32(asdu1);
-    int Imb = SVPublisher_ASDU_addINT32(asdu1);
-    int Imc = SVPublisher_ASDU_addINT32(asdu1);
-    int Imn = SVPublisher_ASDU_addINT32(asdu1);
     int ampaq = SVPublisher_ASDU_addQuality(asdu1);
+    int Imb = SVPublisher_ASDU_addINT32(asdu1);
     int ampbq = SVPublisher_ASDU_addQuality(asdu1);
+    int Imc = SVPublisher_ASDU_addINT32(asdu1);
     int ampcq = SVPublisher_ASDU_addQuality(asdu1);
+    int Imn = SVPublisher_ASDU_addINT32(asdu1);
     int ampnq = SVPublisher_ASDU_addQuality(asdu1);
     int Vma = SVPublisher_ASDU_addINT32(asdu1);
-    int Vmb = SVPublisher_ASDU_addINT32(asdu1);
-    int Vmc = SVPublisher_ASDU_addINT32(asdu1);
-    int Vmn = SVPublisher_ASDU_addINT32(asdu1);
     int Vmaq = SVPublisher_ASDU_addQuality(asdu1);
+    int Vmb = SVPublisher_ASDU_addINT32(asdu1);
     int Vmbq = SVPublisher_ASDU_addQuality(asdu1);
+    int Vmc = SVPublisher_ASDU_addINT32(asdu1);
     int Vmcq = SVPublisher_ASDU_addQuality(asdu1);
+    int Vmn = SVPublisher_ASDU_addINT32(asdu1);
     int Vmnq = SVPublisher_ASDU_addQuality(asdu1);
-    int ts1 = SVPublisher_ASDU_addTimestamp(asdu1);
 
     SVPublisher_ASDU_setSmpCntWrap(asdu1, 4000);
     SVPublisher_ASDU_setRefrTm(asdu1, 0);
 
     SVPublisher_setupComplete(svPublisher);
 
-    clock_t tinit,tfinal;
-
-    double time_used=0.0;
-    double time_theoric = 0.999750;
-    double marge = 250.0e-6;
-    /* ====== Deadline scheduler ====== */
-  //  int runtime = 30000000;
-  //  int deadline = 1e9;
-    /*---- période de la tâche publication de 1s --- */
-//    int period = 1e9;
-    int ret;
+    float time_theoric = 999750.0; //us
+    float marge = 250.0; //us
+        /* ====== Deadline scheduler ====== */
+      /* période de la tâche publication de 1s */
     unsigned int flags =0;
     struct sched_attr attr;
-
-//  affectation des attributs
-    printf("deadline thread started [%ld]\n", gettid());
+    // la tâche commence
+    printf("La tâche d'écriture a commencé id: [%ld]\n", gettid());
 
 	attr.size = sizeof(struct sched_attr);
 	attr.sched_policy = SCHED_DEADLINE;
 	attr.sched_flags = 0;
-	attr.sched_nice = 0;
+	attr.sched_nice = -20;
 	attr.sched_priority = 0;
     attr.sched_policy = SCHED_DEADLINE;
     attr.sched_runtime = 0.9e9;
 	attr.sched_period = attr.sched_deadline = 1.0e9;
-	ret = sched_setattr(0, &attr, flags);
-
-		if (ret < 0) {
-			done = 0;
-			perror("sched_setattr");
-			exit(-1);
-		}
+	sched_setattr(0, &attr, flags);
     /* period in milliseconds */
     struct timespec t;
+    struct timeval maintenant;
+    struct timeval debut_thread;
+    struct timeval debut_programme;
+    long int duration;
     clock_gettime(CLOCK_MONOTONIC, &t);
+    int i=0,taille=30;
+    long int duree [taille];
 
+    gettimeofday(&debut_programme,NULL);
     while (!done) {  /* Boucle infinie */
 
-        tinit = clock();
+        gettimeofday(&debut_thread,NULL);
+        //clock_gettime(CLOCK_MONOTONIC, &debut_thread);
+
         while(*n<4000){
 
         /*verouillage du mutex*/
         pthread_mutex_lock(&(param->mutex));
-        Timestamp ts;
-        Timestamp_clearFlags(&ts);
-        Timestamp_setTimeInMilliseconds(&ts, Hal_getTimeInMs());
 
-        SVPublisher_ASDU_setFLOAT64(asdu1,Ima, *ia);  //écriture dans le bloc de donnée asud1 de la variable courant phase 1
-        SVPublisher_ASDU_setQuality(asdu1,ampaq,q);
-        SVPublisher_ASDU_setFLOAT64(asdu1, Imb,*ib);
+        SVPublisher_ASDU_setSmpSynch(asdu1,2);
+        SVPublisher_ASDU_setINT32(asdu1, Ima,*ia);  //écriture dans le bloc de donnée asud1 de la variable courant phase 1
+        SVPublisher_ASDU_setQuality(asdu1, ampaq,q);
+        SVPublisher_ASDU_setINT32(asdu1, Imb,*ib);
         SVPublisher_ASDU_setQuality(asdu1, ampbq,q);
-        SVPublisher_ASDU_setFLOAT64(asdu1, Imc,*ic);
+        SVPublisher_ASDU_setINT32(asdu1, Imc,*ic);
         SVPublisher_ASDU_setQuality(asdu1, ampcq,q);
-        SVPublisher_ASDU_setFLOAT64(asdu1, Imn,*in);
+        SVPublisher_ASDU_setINT32(asdu1, Imn,*in);
         SVPublisher_ASDU_setQuality(asdu1, ampnq,q);
-        SVPublisher_ASDU_setFLOAT64(asdu1, Vma,*Va);  //écriture dans le bloc de donnée asud1 de la variable courant phase 1
+        SVPublisher_ASDU_setINT32(asdu1, Vma,*Va);  //écriture dans le bloc de donnée asud1 de la variable courant phase 1
         SVPublisher_ASDU_setQuality(asdu1, Vmaq,q);
-        SVPublisher_ASDU_setFLOAT64(asdu1, Vmb,*Vb);
+        SVPublisher_ASDU_setINT32(asdu1, Vmb,*Vb);
         SVPublisher_ASDU_setQuality(asdu1, Vmbq,q);
-        SVPublisher_ASDU_setFLOAT64(asdu1, Vmc,*Vc);
+        SVPublisher_ASDU_setINT32(asdu1, Vmc,*Vc);
         SVPublisher_ASDU_setQuality(asdu1, Vmcq,q);
-        SVPublisher_ASDU_setFLOAT64(asdu1, Vmn,*Vn);
+        SVPublisher_ASDU_setINT32(asdu1, Vmn,*Vn);
         SVPublisher_ASDU_setQuality(asdu1, Vmnq,q);
-        SVPublisher_ASDU_setTimestamp(asdu1,ts1,ts);
-
         SVPublisher_ASDU_setRefrTm(asdu1, Hal_getTimeInMs());
-        SVPublisher_ASDU_increaseSmpCnt(asdu1);
-
-        SVPublisher_publish(svPublisher);
 /*
-        printf("\t sample :  %.2f ", *n );    // vérification sur le terminal des valeurs écrites
-        printf(" Tension Va: %.2f ", *Va);
-        printf(" Tension Vb: %.2f ", *Vb);
-        printf(" Tension Vc: %.2f ", *Vc);
-        printf(" Tension Vn: %.2f ", *Vn);
-        printf(" courant ia: %.2f ", *ia);
-        printf(" courant ib: %.2f ", *ib);
-        printf(" courant ic: %.2f ", *ic);
-        printf(" courant in: %.2f\n",*in);
+        printf("\t sample :  %f ", *n );    // vérification sur le terminal des valeurs écrites
+        printf(" Tension Va: %i ", *Va);
+        printf(" Tension Vb: %i ", *Vb);
+        printf(" Tension Vc: %i ", *Vc);
+        printf(" Tension Vn: %i ", *Vn);
+        printf(" courant ia: %i ", *ia);
+        printf(" courant ib: %i ", *ib);
+        printf(" courant ic: %i ", *ic);
+        printf(" courant in: %i\n",*in);
 */
+        SVPublisher_ASDU_increaseSmpCnt(asdu1);
+        SVPublisher_publish(svPublisher);
         *n+=1; // j'incrémente la valeur de mon échantillon
         /*déverouillage du mutex*/
         pthread_mutex_unlock(&(param->mutex));
@@ -238,25 +231,32 @@ void *publish (void *donnees){
         /* attente le temps d'avoir 250us entre chaque échantillon*/
         time_add_ns(&t,250000);
         }
-
         /*mesure du temps final*/
-        tfinal = clock();
-        time_used = (double)(tfinal-tinit)/(double) CLOCKS_PER_SEC ;
-        /*calcul du temps de cycle*/
-        /* Affichage des différents temps et temps consommes */
-        printf("Nb ticks/seconde = %ld,  Nb ticks depart : %ld, " "Nb ticks final : %ld\n",CLOCKS_PER_SEC, (long)tinit, (long)tfinal);
-        printf("Temps consommé (s) : %lf \n",time_used);
+        gettimeofday(&maintenant,NULL);
+         /*calcul du temps de cycle*/
+        duration = (maintenant.tv_sec - debut_thread.tv_sec);
+        duration *= 1000000;
+        duration += (maintenant.tv_usec - debut_thread.tv_usec);
+        duree [i] =  duration;
 
-       if (time_used < time_theoric - marge || time_used > time_theoric + marge)
-          printf(" erreur : non respect de la durée limite \n");
+        if (maintenant.tv_sec - debut_programme.tv_sec > taille){ // supérieur à une durée fixé dans la variable taille
+            int j;
+            /*-- affichage après un certains temps des temps de cycle --*/
+            for (j=0;j<taille;j++){
 
-        /* réinitialisation --- nouveau cycle */
+              printf(" temps consommée [%i] = %ld nsec \n",j,duree[j]);
+            /* Affichage des différents temps et temps consommes */
+            if (duree[j]< (time_theoric - marge) || duree[j] > (time_theoric + marge))
+                printf(" erreur : non respect de la durée limite \n");
+            }
+            break;
+        }
+         /* réinitialisation --- nouveau cycle */
         *n=0;
-        SVPublisher_ASDU_setSmpCnt(asdu1,0);
+        i+=1;
     }
-
+    /* Cleanup - free all resources */
     SVPublisher_destroy(svPublisher); //destruction publisher
-
     pthread_exit(NULL);
 }
 
@@ -274,63 +274,63 @@ void *create_signal(void *donnees)
     n     = (*param).n;
     double* theta = malloc(sizeof(double));
     theta = (*param).theta;
-    float* Vamp = malloc(sizeof(float));
+    int* Vamp = malloc(sizeof(int));
     Vamp  = (*param).Vamp;
-    float* Iamp = malloc(sizeof(float));
+    int* Iamp = malloc(sizeof(int));
     Iamp  = (*param).Iamp;
-    double* Va = malloc(sizeof(double));
+
+    int* Va = malloc(sizeof(int));
     Va = (*param).Va;
-    double* ia = malloc(sizeof(double));
+    int* ia = malloc(sizeof(int));
     ia = (*param).ia;
-    double* Vb = malloc(sizeof(double));
+    int* Vb = malloc(sizeof(int));
     Vb = (*param).Vb;
-    double* ib = malloc(sizeof(double));
+    int* ib = malloc(sizeof(int));
     ib = (*param).ib;
-    double* Vc = malloc(sizeof(double));
+    int* Vc = malloc(sizeof(int));
     Vc = (*param).Vc;
-    double* ic = malloc(sizeof(double));
+    int* ic = malloc(sizeof(int));
     ic = (*param).ic;
-    double* Vn = malloc(sizeof(double));
+    int* Vn = malloc(sizeof(int));
     Vn = (*param).Vn;
-    double* in = malloc(sizeof(double));
+    int* in = malloc(sizeof(int));
     in = (*param).in;
 
     while(1){ /* Boucle infinie */
 
        /*verrouillage du mutex*/
        pthread_mutex_lock(&(*param).mutex);
-        *theta = *w * (((double) *n) * 1/(*fech));
-        *Va =  *Vamp * sin(*theta);
-        *Vb =  *Vamp * sin(*theta - TWO_PI_OVER_THREE);
-        *Vc =  *Vamp * sin(*theta + TWO_PI_OVER_THREE);
+
+        *theta = *w *(double)(*n * 1/(*fech));
+        *Va =  *Vamp * sin(*theta)*100;
+        *Vb =  *Vamp * sin(*theta - TWO_PI_OVER_THREE)*100;
+        *Vc =  *Vamp * sin(*theta + TWO_PI_OVER_THREE)*100;
         *Vn =  *Va + *Vb + *Vc;
-        *ia =  *Iamp * sin(*theta - *phase);
-        *ib =  *Iamp * sin(*theta - TWO_PI_OVER_THREE -  *phase);
-        *ic =  *Iamp * sin(*theta + TWO_PI_OVER_THREE -  *phase);
+        *ia =  *Iamp * sin(*theta - *phase)*1000;
+        *ib =  *Iamp * sin(*theta - TWO_PI_OVER_THREE -  *phase)*1000;
+        *ic =  *Iamp * sin(*theta + TWO_PI_OVER_THREE -  *phase)*1000;
         *in =  *ia + *ib + *ic;
         /* Deverouillage du mutex */
         pthread_mutex_unlock(&(*param).mutex);
-
     }
         pthread_exit(NULL);
 }
-
 
 int
 main(int argc, char** argv)
 {
     char* Interface;
-    float n = 0.0;
-    float fech=4000.0;
-    double Va,Vb,Vc,Vn,ia,ib,ic,in,theta;
-    float f_nominal =50.0,samplesPerCycle=80.0,f=50.0,w= 2*M_PI*f,Vamp =11000.0,Zmag = 80.0,phase=0.0;
-    float Iamp =Vamp/Zmag;
+    float n=0,fech=4000;
+    int Va,Vb,Vc,Vn,ia,ib,ic,in,Vamp = (int) (11000.0*sqrt(2)),Iamp = (int) (20.0*sqrt(2));
+    double theta = 0.0;
+    float f_nominal =50.0,samplesPerCycle=80.0,f=50.0,w= 2*M_PI*f,phase=M_PI/6;
 
     if (argc > 1)
         Interface = argv[1];
     else
         Interface = "eth0";
     printf("Using Interface : %s \n",Interface);
+
     // création d'un bloc de données de thread de types data_
     data_ thread_data;
 
@@ -354,49 +354,24 @@ main(int argc, char** argv)
     thread_data.phase=&phase;
     thread_data.samplesPerCycle = &samplesPerCycle;
     thread_data.fech = &fech;
-    thread_data.in = &in;
     thread_data.theta = &theta;
     thread_data.interface = Interface;
 
     //intalisation du mutex
     pthread_mutex_init(&thread_data.mutex,NULL);
 
-    // déclaration des threads
+    // décalaration des threads
     pthread_t thread_publish;
     pthread_t thread_create_signal;
 
-    // priorité thread
-
-    pthread_attr_t attr1,attr2; 		//attributs pour gérer les priorités
-	struct sched_param sched1,sched2;	//gestionnaire de priorité
-    sched1.sched_priority = 99;
-    sched2.sched_priority = 1;
-
-	pthread_attr_init(&attr1); 		//initialise les attributs par défauts
-	pthread_attr_init(&attr2);
-
-	sched_setscheduler(0, SCHED_FIFO, &sched1); //on instancie les gestionnaires
-	sched_setscheduler(0, SCHED_FIFO, &sched2);
-
-	pthread_attr_setschedparam(&attr1,&sched1); //on initialise les attributs en fonction des gestionnaires
-	pthread_attr_setschedparam(&attr2,&sched2);
-
-
     // création des threads de publication et création de signaux;
-    pthread_create(&thread_publish,&attr1,publish,&thread_data);
-    pthread_create(&thread_create_signal,&attr2,create_signal,&thread_data);
+    pthread_create(&thread_publish,NULL,publish,&thread_data);
+    pthread_create(&thread_create_signal,NULL,create_signal,&thread_data);
 
     // le programme principal attend la fin des deux tâches
     pthread_join(thread_publish,NULL);
     pthread_join(thread_create_signal,NULL);
 
+
     return 0;
 } /* main() */
-
-
-
-
-
-
-
-
